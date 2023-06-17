e462b8ef3e0754514a4b7252216efe427cc54a25
dmitri <dmitribazilov@gmail.com>
2023-06-17 05:19:59 +0300

    add priority view

--- app/src/main/java/com/dmitri/yandex_tasks/TaskApplication.kt
+++ app/src/main/java/com/dmitri/yandex_tasks/TaskApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.dmitri.yandex_tasks.util.repository.TodoItemsRepository
 
 class TaskApplication : Application() {
-
+    //todo appbar colorimportance
     val repository = TodoItemsRepository()
 
     override fun onCreate() {
